c4e67c2e45439b0314d7f8f253308b2a8973c2c1
Izmar <izmaelverhage@gmail.com>
2019-03-10 15:15:51 +0100

    Adds first commit

---
+++ src/test/test.cpp
@@ -0,0 +1,13 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+#include <Mpc.hpp>
+#include <sequencer/Sequencer.hpp>
+
+using namespace mpc;
+
+TEST_CASE("Mpc is instantiated and booted", "[mpc-boot]") {
+	Mpc mpc;
+	mpc.init("rtaudio", 44100);
+	REQUIRE(mpc.getSequencer().lock());
+}
