effe51890284d8c0b55bb5715c353075be75169a
Izmar Verhage <izmaelverhage@gmail.com>
2019-05-13 22:06:37 +0200

    Fix more controls issues

--- src/main/cpp/Gui.cpp
+++ src/main/cpp/Gui.cpp
@@ -153,92 +153,92 @@ void Gui::setUserScale(const float& userScale) {
 
 void Gui::handleKeyDown(const SDL_KeyboardEvent& event) {
 	auto hw = mpc->getHardware().lock();
-    switch(event.keysym.sym){
-		case SDLK_F1:
-			hw->getButton("f1").lock()->push();
-			break;
-		case SDLK_F2:
-			hw->getButton("f2").lock()->push();
-			break;
-		case SDLK_F3:
-			hw->getButton("f3").lock()->push();
-			break;
-		case SDLK_F4:
-			hw->getButton("f4").lock()->push();
-			break;
-		case SDLK_F5:
-			hw->getButton("f5").lock()->push();
-			break;
-		case SDLK_F6:
-			hw->getButton("f6").lock()->push();
-			break;
-		case SDLK_ESCAPE:
-			hw->getButton("mainscreen").lock()->push();
-			break;
-		case SDLK_i:
-			hw->getButton("openwindow").lock()->push();
-			break;
-		case SDLK_LSHIFT:
-			hw->getButton("shift").lock()->push();
-			break;
-		case SDLK_EQUALS:
-			if (event.keysym.mod == KMOD_LCTRL) {
-				if (userScale == SMALL) {
-					setUserScale(MEDIUM);
-				}
-				else if (userScale == MEDIUM) {
-					setUserScale(LARGE);
-				}
+	switch (event.keysym.sym) {
+	case SDLK_F1:
+		hw->getButton("f1").lock()->push();
+		break;
+	case SDLK_F2:
+		hw->getButton("f2").lock()->push();
+		break;
+	case SDLK_F3:
+		hw->getButton("f3").lock()->push();
+		break;
+	case SDLK_F4:
+		hw->getButton("f4").lock()->push();
+		break;
+	case SDLK_F5:
+		hw->getButton("f5").lock()->push();
+		break;
+	case SDLK_F6:
+		hw->getButton("f6").lock()->push();
+		break;
+	case SDLK_ESCAPE:
+		hw->getButton("mainscreen").lock()->push();
+		break;
+	case SDLK_i:
+		hw->getButton("openwindow").lock()->push();
+		break;
+	case SDLK_LSHIFT:
+		hw->getButton("shift").lock()->push();
+		break;
+	case SDLK_EQUALS:
+		if (event.keysym.mod == KMOD_LCTRL) {
+			if (userScale == SMALL) {
+				setUserScale(MEDIUM);
+			}
+			else if (userScale == MEDIUM) {
+				setUserScale(LARGE);
+			}
+		}
+		else {
+			if (event.keysym.mod == KMOD_LSHIFT) {
+				hw->getDataWheel().lock()->turn(10);
 			}
 			else {
-				if (event.keysym.mod == KMOD_LSHIFT) {
-					hw->getDataWheel().lock()->turn(10);
-				}
-				else {
-					hw->getDataWheel().lock()->turn(1);
-				}
+				hw->getDataWheel().lock()->turn(1);
 			}
-			break;
-		case SDLK_MINUS:
-			if (event.keysym.mod == KMOD_LCTRL) {
-				if (userScale == LARGE) {
-					setUserScale(MEDIUM);
-				}
-				else if (userScale == MEDIUM) {
-					setUserScale(SMALL);
-				}
+		}
+		break;
+	case SDLK_MINUS:
+		if (event.keysym.mod == KMOD_LCTRL) {
+			if (userScale == LARGE) {
+				setUserScale(MEDIUM);
+			}
+			else if (userScale == MEDIUM) {
+				setUserScale(SMALL);
+			}
+		}
+		else {
+			if (event.keysym.mod == KMOD_LSHIFT) {
+				hw->getDataWheel().lock()->turn(-10);
 			}
 			else {
-				if (event.keysym.mod == KMOD_LSHIFT) {
-					hw->getDataWheel().lock()->turn(-10);
-				}
-				else {
-					hw->getDataWheel().lock()->turn(-1);
-				}
+				hw->getDataWheel().lock()->turn(-1);
 			}
-			break;
-		case SDLK_LEFT:
-			hw->getButton("left").lock()->push();
-            break;
-        case SDLK_UP:
-			hw->getButton("up").lock()->push();
-			break;
-        case SDLK_RIGHT:
-			hw->getButton("right").lock()->push();
-			break;
-        case SDLK_DOWN:
-			hw->getButton("down").lock()->push();
-			break;
-		case SDLK_z:
-			hw->getPad(0).lock()->push(127);
-			break;
-		case SDLK_x:
-			hw->getPad(1).lock()->push(127);
-			break;
-		case SDLK_c:
-			hw->getPad(2).lock()->push(127);
-			break;
-    }
+		}
+		break;
+	case SDLK_LEFT:
+		hw->getButton("left").lock()->push();
+		break;
+	case SDLK_UP:
+		hw->getButton("up").lock()->push();
+		break;
+	case SDLK_RIGHT:
+		hw->getButton("right").lock()->push();
+		break;
+	case SDLK_DOWN:
+		hw->getButton("down").lock()->push();
+		break;
+	case SDLK_z:
+		hw->getPad(0).lock()->push(127);
+		break;
+	case SDLK_x:
+		hw->getPad(1).lock()->push(127);
+		break;
+	case SDLK_c:
+		hw->getPad(2).lock()->push(127);
+		break;
+	}
 	mpc->getLayeredScreen().lock()->Draw();
 	rootComponent->draw(cairoContext, false);
 	draw();
