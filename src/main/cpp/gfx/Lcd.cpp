28c9dc9f9d9c8467ec8c752dab52af2e3ba473c6
Izmar Verhage <izmaelverhage@gmail.com>
2019-05-13 19:28:41 +0200

    Draw lcd onto a 496 x 120 surface with grid

--- src/main/cpp/gfx/Lcd.cpp
+++ src/main/cpp/gfx/Lcd.cpp
@@ -4,7 +4,7 @@ Lcd::Lcd(const MRECT& rect, vector<vector<bool>>* pixels, const string& name)
 	: Component(rect, name)
 {
 	this->pixels = pixels;
-	surface = cairo_image_surface_create(CAIRO_FORMAT_RGB24, 248, 60);
+	surface = cairo_image_surface_create(CAIRO_FORMAT_RGB24, 248 * 2, 60 * 2);
 	data = cairo_image_surface_get_data(surface);
 }
 
@@ -12,22 +12,32 @@ void Lcd::draw(cairo_t* context, bool dirtyOnly) {
 	if (!dirtyOnly || (dirtyOnly && dirty)) {
 		prepareClip(context);
 		prepareTranslate(context);
-
+		const float lightnessFactor = 2.0f;
+		MColorF fgColor2 = { 1.0f, fgColor.R * lightnessFactor, fgColor.G * lightnessFactor, fgColor.B * lightnessFactor };
 		for (int y = 0; y < 60; y++) {
 			for (int x = 0; x < 248; x++) {
 				const bool pixel = (*pixels)[x][y];
-				int offset = ((y * 248) + x) * 4;
+				int offset1 = ((y * 496 * 2) + (x * 2)) * 4;
+				int offset2 = ((y * 496 * 2) + (x * 2) + 1) * 4;
+				int offset3 = ((y * 496 * 2) + 496 + (x * 2)) * 4;
+				int offset4 = ((y * 496 * 2) + 496 + (x * 2) + 1) * 4;
 				if (pixel) {
-					setPixel3F(data + offset, fgColor);
+					setPixel3F(data + offset1, fgColor);
+					setPixel3F(data + offset2, fgColor2);
+					setPixel3F(data + offset3, fgColor2);
+					setPixel3F(data + offset4, fgColor2);
 				}
 				else {
-					setPixel3F(data + offset, bgColor);
+					setPixel3F(data + offset1, bgColor);
+					setPixel3F(data + offset2, bgColor);
+					setPixel3F(data + offset3, bgColor);
+					setPixel3F(data + offset4, bgColor);
 				}
 			}
 		}
 		
+		cairo_scale(context, 0.5f, 0.5f);
 		cairo_set_source_surface(context, surface, 0, 0);
-
 		cairo_paint(context);
 
 		restoreTranslate(context);
