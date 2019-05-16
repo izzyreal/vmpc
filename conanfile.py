4b0b68b0821aea04b36b27366c8a1ebde943e942
Izmar Verhage <izmar.verhage@lunatech.nl>
2019-05-16 12:25:24 +0200

    Use updated rtaudio

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class Pkg(ConanFile):
     settings = "os", "compiler", "arch", "build_type"
     generators = "cmake"
     exports_sources = "src/*"
-    requires = ("log4cplus/2.0.0-rc2@bincrafters/stable", "RtAudio/master@qno/testing", "portaudio/v190600.20161030@bincrafters/stable", "cairo/1.15.14@bincrafters/stable", "sdl2/2.0.9@bincrafters/stable", "sdl2_image/2.0.4@bincrafters/stable", "mpc/0.1@izmar/dev", "ctoot/0.1@izmar/dev", "moduru/0.1@izmar/dev", "rapidjson/1.1.0@bincrafters/stable", "Catch2/2.7.0@catchorg/stable")
+    requires = ("log4cplus/2.0.0-rc2@bincrafters/stable", "RtAudio/5.1.0@qno/testing", "portaudio/v190600.20161030@bincrafters/stable", "cairo/1.15.14@bincrafters/stable", "sdl2/2.0.9@bincrafters/stable", "sdl2_image/2.0.4@bincrafters/stable", "mpc/0.1@izmar/dev", "ctoot/0.1@izmar/dev", "moduru/0.1@izmar/dev", "rapidjson/1.1.0@bincrafters/stable", "Catch2/2.7.0@catchorg/stable")
     url = "https://github.com/izzyreal/vmpc.git"
 
     def build(self):
